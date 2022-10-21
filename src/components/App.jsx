9e5ec546780850536ae0c291e08c104776d68946
Denis Kyzym <102521716+kyzym@users.noreply.github.com>
2022-10-21 07:49:40 +0200

    4th

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import user from "./data/user.json"
 import stats from "./data/stat-data.json"
 import friends from "./data/friends.json";
+import transactions from "./data/transactions.json";
 import { Profile } from "components/Profile/Profile"
 import { Statistics } from "components/Statistics/Statistics"
 import { FriendList} from "./FriendList/FriendList";
+import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
 
 export const App = () => {
     return <>
@@ -12,7 +14,8 @@ export const App = () => {
         />
         <Statistics title="Upload stats" stats={stats} />
         <Statistics stats={stats} />
-        <FriendList friends={friends} />;
+        <FriendList friends={friends} />
+        <TransactionHistory items={transactions} />
 
 </>
 
--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,7 +1,19 @@
-import {FriendListItem} from "./FriendListItem"
+import PropTypes from 'prop-types';
+import { FriendListItem } from "./FriendListItem"
 
 export const FriendList = ({ friends }) => {
     return <ul className="friend-list">
    {friends.map(friend=>(<FriendListItem {...friend} key={friend.id}></FriendListItem>))}
 </ul>
+}
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ).isRequired,
 }
